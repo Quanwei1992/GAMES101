0b9a9cb7706df7ce2afc61c87d453ddbef70c3a8
Quanwei <573011406@qq.com>
2020-08-24 15:52:37 +0800

    完成作业1

--- source/01-Transformation/main.cpp
+++ source/01-Transformation/main.cpp
@@ -1,48 +1,35 @@
+﻿
+/*
+* 作业描述
+    给定一个点 P=(2,1), 将该点绕原点先逆时针旋转 45◦，再平移 (1,2), 计算出
+变换后点的坐标（要求用齐次坐标进行计算）。
+*/
+
+
 #include<cmath>
 #include<eigen3/Eigen/Core>
 #include<eigen3/Eigen/Dense>
 #include<iostream>
 
+#define PI 3.14159265f
+#define DEG2RAD(x) ((x)*((PI * 2.0f) / 360.0f))
+using namespace Eigen;
+
 int main(){
 
-    // Basic Example of cpp
-    std::cout << "Example of cpp \n";
-    float a = 1.0, b = 2.0;
-    std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
-    std::cout << std::sqrt(b) << std::endl;
-    std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
 
-    // Example of vector
-    std::cout << "Example of vector \n";
-    // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
-    // vector output
-    std::cout << "Example of output \n";
-    std::cout << v << std::endl;
-    // vector add
-    std::cout << "Example of add \n";
-    std::cout << v + w << std::endl;
-    // vector scalar multiply
-    std::cout << "Example of scalar multiply \n";
-    std::cout << v * 3.0f << std::endl;
-    std::cout << 2.0f * v << std::endl;
+	float rad = DEG2RAD(45.0f);
 
-    // Example of matrix
-    std::cout << "Example of matrix \n";
-    // matrix definition
-    Eigen::Matrix3f i,j;
-    i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
-    j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
-    // matrix output
-    std::cout << "Example of output \n";
-    std::cout << i << std::endl;
-    // matrix add i + j
-    // matrix scalar multiply i * 2.0
-    // matrix multiply i * j
-    // matrix multiply vector i * v
+	Vector3f v(1, 0, 1.0f);
+
+	Matrix3f m;
+	m << cos(rad), -sin(rad), 0,
+		 sin(rad),  cos(rad), 0,
+		        0,         0, 1;
+
+	Vector3f rv = m * v;
+
+	std::cout << rv;
 
     return 0;
 }
