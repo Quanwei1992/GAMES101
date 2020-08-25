c2bdb8a30067f2a9c5df0f4daa26ec1c5da9ec7d
Quanwei <573011406@qq.com>
2020-08-25 14:53:17 +0800

    Homework 0

---
+++ 00/main.cpp
@@ -0,0 +1,26 @@
+
+
+#include<cmath>
+#include<eigen3/Eigen/Core>
+#include<eigen3/Eigen/Dense>
+#include<iostream>
+
+#define PI 3.1415926f
+#define DEG2RAD (PI/180.0f)
+
+int main()
+{
+
+    Eigen::Vector3f P(2,1,1);
+    Eigen::Matrix3f M;
+
+    float rad = 45.0f * DEG2RAD;
+    M << cos(rad),-sin(rad),1,
+         sin(rad),cos(rad) ,2,
+         0,0,1;
+    
+    Eigen::Vector3f P2 = M * P;
+    std::cout<<P2<<std::endl;
+
+    return 0;
+}
